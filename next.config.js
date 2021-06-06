3b55f9d019b4ce99ed64cd9f22f2bd72f9a9a9d0
Woody <kazettique@gmail.com>
2021-06-06 22:17:05 +0800

    update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  basePath: "/next-deploy-yarn-test/",
-  assetPrefix: "/next-deploy-yarn-test/",
+  basePath: "/next-deploy-yarn-test",
+  assetPrefix: "/next-deploy-yarn-test",
 };
