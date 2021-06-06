7c3e69b93b09e0fbd4a6cedaf658fee891ad6ef1
Woody <kazettique@gmail.com>
2021-06-06 21:28:54 +0800

    update config

---
+++ .env-config.js
@@ -0,0 +1,5 @@
+// const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  "process.env.NEXT_PUBLIC_BASE_PATH": "/next-deploy-npm-test",
+};
--- next.config.js
+++ next.config.js
@@ -3,4 +3,6 @@ module.exports = {
   future: {
     webpack5: true,
   },
-}
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+};
